74ec53ada85762e44c4fc7bc747ff26151001c83
Itserge1 <realitserge@gmail.com>
2023-05-30 20:55:51 -0400

    Update gitignore file

--- backend/flask_app/controllers/Dealership.py
+++ backend/flask_app/controllers/Dealership.py
@@ -1,8 +1,9 @@
 from flask_app import app
-from flask_app import jsonify
+from flask_app import jsonify, os
 
 @app.route("/api")
 def Home():
+    print(os.getenv("API_LINK_KEY"))
     greet = {
         "home": "Welcome home : )"
     }
